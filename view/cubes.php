b34033e50974c3cddbb7b58462e7cbcc60e013f8
jeffersonpatron <jeffersonpatron@gmail.com>
2015-09-17 11:56:54 -0500

    Create cubes.php

---
+++ view/cubes.php
@@ -0,0 +1,26 @@
+<?php
+if($message){
+	$mensaje = $message;
+}else{
+	$mensaje = "";
+}
+echo '<html>
+<head>
+<link rel="stylesheet" href="includes/css/style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<meta charset="UTF-8"> 
+<title>'.$title.'</title>
+</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#" alt="Cube Summation" title="Prueba de Cube Summation">C.S</a>
+    </div>
+  </div>
+</nav>
+</body>
+
+';
+
+?>
