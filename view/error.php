b6fa1fcc1c1f5ad757d84efc68c7adb9a38df625
jeffersonpatron <jeffersonpatron@gmail.com>
2015-09-17 11:57:10 -0500

    Create error.php

---
+++ view/error.php
@@ -0,0 +1,20 @@
+<?php 
+echo '<html>
+<head>
+<link rel="stylesheet" href="includes/css/style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<meta charset="UTF-8"> 
+<title>'.$title.'</title>
+</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#" alt="Cube Summation" title="Prueba de Cube Summation">C.S</a>
+    </div>
+  </div>
+</nav>
+</body>
+
+';
+$_SESSION['mensaje'] = $message;
